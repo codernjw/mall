1a7a0e974f3ebab69e8ded2130bad14d0574167a
codernjw <1429371466@qq.com>
2021-08-02 17:44:11 +0800

    main

---
+++ src/components/tabbar/Tabbar.vue
@@ -0,0 +1,18 @@
+<template>
+   <div id="tab-bar">
+       <div class="tab-bar-item">首页</div>
+       <div class="tab-bar-item">分类</div>
+       <div class="tab-bar-item">购物车</div>
+       <div class="tab-bar-item">我的</div>
+   </div>
+</template>
+
+<script>
+export default {
+name:'Tabbar',
+}
+</script>
+
+<style>
+@import '../../assets/css/tab-bar.css';
+</style>
