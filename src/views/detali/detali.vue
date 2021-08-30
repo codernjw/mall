38de925c1260169bc204bd4933b0b7b388be98f3
codernjw <1429371466@qq.com>
2021-08-30 21:03:00 +0800

    main

--- src/views/detali/detali.vue
+++ src/views/detali/detali.vue
@@ -3,6 +3,59 @@
      <detalinavbar></detalinavbar>
      <detaliswiper :topimage="topimage"></detaliswiper>
      <detalibaseinfo :goods="goods"></detalibaseinfo>
+     <detail-shop-info :shop="shop"></detail-shop-info>
+        <ul>
+       <li>1</li>
+       <li>2</li>
+       <li>3</li>
+       <li>4</li>
+       <li>5</li>
+       <li>6</li>
+       <li>7</li>
+       <li>8</li>
+       <li>9</li>
+       <li>10</li>
+       <li>11</li>
+       <li>12</li>
+       <li>13</li>
+       <li>14</li>
+       <li>15</li>
+       <li>16</li>
+       <li>17</li>
+       <li>18</li>
+       <li>19</li>
+       <li>20</li>
+       <li>21</li>
+       <li>22</li>
+       <li>23</li>
+       <li>24</li>
+       <li>25</li>
+       <li>26</li>
+       <li>27</li>
+       <li>28</li>
+       <li>29</li>
+       <li>30</li>
+       <li>31</li>
+       <li>32</li>
+       <li>33</li>
+       <li>34</li>
+       <li>35</li>
+       <li>36</li>
+       <li>37</li>
+       <li>38</li>
+       <li>39</li>
+       <li>40</li>
+       <li>41</li>
+       <li>42</li>
+       <li>43</li>
+       <li>44</li>
+       <li>45</li>
+       <li>46</li>
+       <li>47</li>
+       <li>48</li>
+       <li>49</li>
+       <li>50</li>
+   </ul>
   </div>
 </template>
 
@@ -10,8 +63,9 @@
 import detalinavbar from '../detali/childcomps/detalinavbar.vue'
 import detaliswiper from './childcomps/detaliswiper.vue'
 import detalibaseinfo from './childcomps/detalibaseinfo.vue'
+import DetailShopInfo from './childcomps/DetailShopInfo.vue'
 
-import {getdetali,Goods} from '../../network/detali'
+import {getdetali,Goods ,Shop} from '../../network/detali'
 
 export default {
 name:'detali',
@@ -19,12 +73,14 @@ components: {
    detalinavbar,
    detaliswiper,
    detalibaseinfo,
+   DetailShopInfo,
 },
 data(){
     return {
         iid:null,
         topimage:[],
         goods:{},
+        shop:{},
     }
 },
 created () {
@@ -32,9 +88,11 @@ created () {
     getdetali(this.iid).then(res=>{
         const data=res.result;
        this.topimage=data.itemInfo.topImages;
-       console.log(res);
+    //    console.log(res);
        this.goods=new Goods(data.itemInfo,data.columns,data.shopInfo.services);
-       console.log(this.goods);
+    //    console.log(this.goods);
+       this.shop=new Shop(data.shopInfo);
+       console.log(this.shop);
     })
 },
 }
